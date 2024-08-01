475ed6cb47c9f5264a9d403f1cbaf16923076505
laraaskovic <lara.askovic@gmail.com>
2024-07-31 21:33:59 -0400

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -98,7 +98,7 @@ function handleImageError() {
                                         <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
 
                                         <p class="mt-4 text-sm/relaxed">
-                                            Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                            Laravel has clean documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                         </p>
                                     </div>
                                 </div>
