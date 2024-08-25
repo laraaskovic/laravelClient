21e363ce0fc9da63da26d97e88929da6c11e0b7e
laraaskovic <lara.askovic@gmail.com>
2024-08-25 11:22:28 -0400

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -119,8 +119,6 @@ function handleImageError() {
 
 
 
-
-
                             <svg class="size-6 shrink-0 stroke-[#4169E1]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/></svg>
                                 <!-- Changes the arrow colour-->
 
